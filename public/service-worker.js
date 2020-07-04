a11307ef23df8201a8bb325f656bb4576431708f
Ana Marr <acarolinasim@gmail.com>
2020-07-04 21:23:37 +0930

    files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,6 @@
-const FILES_TO_CACHE = [
-  "/", 
+const FILES_TO_CACHE = [ 
   "./index.html", 
   "./index.js", 
-  "./favicon.ico",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
